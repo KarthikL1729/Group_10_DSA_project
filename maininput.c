6de07581b93a2a6898e48e355ce605d1ddf21668
KarthikL1729 <71184604+KarthikL1729@users.noreply.github.com>
2021-05-08 21:25:22 +0530

    Fixed maininput.c

--- maininput.c
+++ maininput.c
@@ -26,12 +26,6 @@ int main()
     {
         inputRoad(); //Function for taking the inputs to the stations graph. Needs to be updated (Struct station hasn't been defined properly yet).
     }
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-  
->>>>>>> 798b64e0a41c8ba792b000423493229bb4d667ff
     person arrPerson[K]; //For storing the [ struct person ]  i.e, details about each person
     
     // taking the initial locations of each person in order 
@@ -40,16 +34,6 @@ int main()
     for (int i = 0; i < K; i++)
     {
         scanf("%d", &arrPerson[i].cur_station);
-<<<<<<< HEAD
-=======
-    person arrPerson[K]; 
-
-    for(int i=0;i<K;i++)
-    {   
-        scanf("%d",&arrPerson[i].cur_station);
->>>>>>> 947f7f56ecfb107188e4b8bf8a095e7f48ae9b31
-=======
->>>>>>> 798b64e0a41c8ba792b000423493229bb4d667ff
     }
 
     int selectTask = -1;
@@ -76,11 +60,7 @@ int main()
                 scanf("%d", &listPostive[i]);
             }
 
-<<<<<<< HEAD
             Task1(listPostive,numPositive,arrPerson,X,D,K); //Needs to be defined.
-=======
-            Task1(listPostive, X, D); //Needs to be defined.
->>>>>>> f707e5edfd2c562f98d3cc42a398b345e9c06a9e
         }
 
         if (selectTask == 2) 
