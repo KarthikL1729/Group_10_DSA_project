7897ac81b400d463bbb9cdf7f2b395337720f396
KarthikL1729 <71184604+KarthikL1729@users.noreply.github.com>
2021-05-08 15:35:29 +0530

    Added day variable to struct person

--- prelims.h
+++ prelims.h
@@ -8,10 +8,9 @@ typedef struct person{
     int status; //0, is negative, 1 is primary, 2 is secondary, and 3 is positive. 
     /*Will update status according to the day given.
         Not completely clear yet*/
+    int day;  // Day he/she tested positive, initially will be -1.
     int cur_station; //current station
 }person;
 
 person arr[];
 
-
-// Sreeharsha , adding a comment for trial
